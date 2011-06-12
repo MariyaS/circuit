2117575811b164f39a58be786becf01046b8e640
Nick Watts <nick@nawatts.com>
2011-06-12 18:23:18 +0000

    Changed layout to one label per column. Second line is taken up by current and voltage color labels.

--- src/OscilloscopeLayout.java
+++ src/OscilloscopeLayout.java
@@ -28,11 +28,15 @@ class OscilloscopeLayout implements LayoutManager {
 		for ( int i = 1; i < target.getComponentCount(); i++ ) {
 			Component c = target.getComponent(i);
 			if ( c instanceof JLabel ) {
+				
+				/* Two per column
 				int x = insets.left+5 + nLabels/2 * 80;
 				int y = insets.top;
 				if ( nLabels % 2 == 1 ) {
 					y = insets.top+15;
-				}
+				} */
+				int x = insets.left+5 + nLabels * c.getPreferredSize().width;
+				int y = insets.top;
 				c.setLocation(x, y);
 				c.setSize(c.getPreferredSize());
 				nLabels++;
