be5fc7e0b1e71374754d496e6475b8f0b391c0ac
Nick Watts <nick@nawatts.com>
2011-07-01 15:47:01 +0000

    Made the height of the label area at the top of the window and the info area at the bottom of the window variables so they can be easily changed in only one place.

--- src/OscilloscopeLayout.java
+++ src/OscilloscopeLayout.java
@@ -2,31 +2,32 @@ import java.awt.*;
 import javax.swing.*;
 
 class OscilloscopeLayout implements LayoutManager {
+	
+	public static final int LABEL_AREA_HEIGHT = 40;
+	public static final int INFO_AREA_HEIGHT = 50;
+	
 	public OscilloscopeLayout() {}
 	
-	public Dimension preferredLayoutSize(Container target) {
-		return new Dimension(350,450);
-	}
-	public Dimension minimumLayoutSize(Container target) {
-		return new Dimension(200,250);
-	}
+	public Dimension preferredLayoutSize(Container target) { return new Dimension(600,450); }
+	public Dimension minimumLayoutSize(Container target) { return new Dimension(500,300); }
+	
 	public void addLayoutComponent(String name, Component c) {}
 	public void removeLayoutComponent(Component c) {}
 	
-	public void layoutContainer(Container target) {
-		Insets insets = target.getInsets();
-		int targetw = target.getWidth() - (insets.left + insets.right);
-		int targeth = target.getHeight() - (insets.top + insets.bottom);
+	public void layoutContainer(Container window) {
+		Insets insets = window.getInsets();
+		int window_width = window.getWidth() - (insets.left + insets.right);
+		int window_height = window.getHeight() - (insets.top + insets.bottom);
 		 		
-		// Position canvas
-		Component canvas = target.getComponent(0);
-		canvas.setLocation(insets.left, insets.top+40);
-		canvas.setSize(targetw, targeth-80);
+		// Layout canvas
+		Component canvas = window.getComponent(0);
+		canvas.setLocation(insets.left, insets.top+LABEL_AREA_HEIGHT);
+		canvas.setSize(window_width, window_height-(LABEL_AREA_HEIGHT+INFO_AREA_HEIGHT));
 		
-		// Position component labels
+		// Layout component labels
 		int nLabels = 0;
-		for ( int i = 1; i < target.getComponentCount(); i++ ) {
-			Component c = target.getComponent(i);
+		for ( int i = 1; i < window.getComponentCount(); i++ ) {
+			Component c = window.getComponent(i);
 			if ( c instanceof JLabel ) {
 				int x = insets.left+5 + nLabels * c.getPreferredSize().width;
 				int y = insets.top;
